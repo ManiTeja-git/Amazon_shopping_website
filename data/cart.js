9e0944bb3905e06323f848553b2569d2d5136618
Mani <manitejaaerupula@gmail.com>
2024-06-30 20:37:53 +0530

    delete products from cart whenc clciking delete

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,4 @@
-export const cart = [
+export let cart = [
   {
     productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     quantity: 2,
@@ -8,6 +8,7 @@ export const cart = [
     quantity: 1,
   },
 ];
+
 export function addToCart(productId) {
   let matchingItem;
 
@@ -28,4 +29,14 @@ export function addToCart(productId) {
       quantity,
     });
   }
+}
+
+export function removeFromCart(productId) {
+  const newCart = [];
+  cart.forEach((cartItem) => {
+    if (productId !== cartItem.productId) {
+      newCart.push(cartItem);
+    }
+  });
+  cart = newCart;
 }
